faed2e368c45ebe07a907a40c960d2e8d9b282d0
Adrian Gonzalez <adrianandgonzalez@gmail.com>
2018-08-13 10:00:58 -0700

    added styles

--- public/js/index.js
+++ public/js/index.js
@@ -36,13 +36,15 @@ socket.on(`newLocationMessage`, function(locationMessage) {
 //   console.log('Got it!', data);
 // });
 
+var messageTextbox = jQuery('[name=message]');
+
 jQuery('#message-form').on('submit', function(e) {
   e.preventDefault();
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   }, function () {
-
+    messageTextbox.val('');
   });
 });
 
@@ -52,12 +54,16 @@ locationButton.on('click', function() {
     return alert('Gelocation not supported by your browser');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
   navigator.geolocation.getCurrentPosition(function(position) {
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function(e) {
-    alert('Unable to fetch location')
+    alert('Unable to fetch location');
+    locationButton.removeAttr('disabled').text('Send location');
   });
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,21 +1,24 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message')
+var {generateMessage, generateLocationMessage} = require('./message')
 
 describe('generateMessage', () => {
   it('Should generate the correct message object', () => {
     var mesObj = generateMessage('Beavis', 'Cornholio');
 
-    console.log(mesObj.from);
 
-    expect(typeof mesObj.createdAt).toBe("number");
+    expect(typeof mesObj.createdAt).toBe('number');
     expect(mesObj.from).toBe('Beavis');
     expect(mesObj.text).toBe('Cornholio');
   });
 });
 
 describe('generateLocationMessage', () => {
-  it('Should generate a message with a location', () => {
+  it('Should generate correct location object', () => {
+    var locObj = generateLocationMessage("Adrian", 0, 0);
 
+    expect(typeof locObj.createdAt).toBe('number');
+    expect(locObj.from).toBe('Adrian');
+    expect(locObj.url).toBe('https://www.google.com/maps?q=0,0');
   });
 });
